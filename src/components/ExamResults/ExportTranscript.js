36cdfbbdead6867fa138966524f6b4c73ea6fa9e
phamquanghien <pqh.cntt@gmail.com>
2024-05-15 17:54:36 +0700

    Done

--- src/components/ExamResults/ExportTranscript.js
+++ src/components/ExamResults/ExportTranscript.js
@@ -1,11 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Table, Form, Button, Col, InputGroup } from 'react-bootstrap';
+import { FaSearch } from 'react-icons/fa';
+import { FaFileDownload } from "react-icons/fa";
+import { useParams } from 'react-router-dom';
 
 const ExportTranscript = () => {
-    return (
-        <div>
-            <h1>Export Transcript</h1>
-        </div>
-    );
+    const { examId } = useParams();
+  const [keySearch, setKeySearch] = useState("");
+  const [subjectExam, setSubjectExam] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isNotFound, setIsNotFound] = useState(false);
+  const fetchData = async () => {
+    setIsLoading(false);
+    setIsNotFound(false);
+    try {
+      const result = await axios.get(`http://localhost:5107/api/SubjectExam/employee-get-by-exam-bag?examID=${examId}&examBag=${keySearch}`);
+      setSubjectExam(result.data);
+      setIsLoading(true);
+    } catch (error) {
+      console.error(error);
+      setIsNotFound(true);
+    } finally {
+    }
+  };
+  const handleSearch = () => {
+    if (keySearch !== "") {
+      fetchData();
+    }
+  };
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' && keySearch !== "") {
+      handleSearch();
+    }
+  };
+  const handleChange = (event) => {
+    setKeySearch(event.target.value);
+  };
+  const handleDownload = async () => {
+    const response = await axios.get(`http://localhost:5107/api/ExamResult/admin-download-by-exam-bag?examID=${examId}&examBag=${keySearch}`, {
+        responseType: 'blob',
+    });
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'Diem_Tui_Bai_Thi' + keySearch + '.xlsx'); // Đặt tên file mặc định
+    document.body.appendChild(link);
+    link.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+  return (
+    <div className='m-3'>
+      <Col className="mb-3" md={6} lg={3}>
+        <InputGroup>
+          <Form.Control
+            type="text" 
+            placeholder="Nhập túi bài thi để tìm kiếm"
+            onChange={handleChange} 
+            onKeyDown={handleKeyDown}
+          />
+          <Button onClick={handleSearch}>
+            <FaSearch />
+          </Button>
+        </InputGroup>
+      </Col>
+      {isLoading && (
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th className="text-center">Mã môn</th>
+              <th className="text-center">Tên môn</th>
+              <th className="text-center">Túi bài thi</th>
+              <th className="text-center">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {subjectExam && (
+              <tr key={subjectExam.subjectExamID}>
+                <td className="text-center">{subjectExam.subjectCode}</td>
+                <td className="text-center">{subjectExam.subject && subjectExam.subject.subjectName}</td>
+                <td className="text-center">{subjectExam.examBag}</td>
+                <td className="text-center text-primary">
+                  <FaFileDownload onClick={handleDownload}/>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      )}
+      {isNotFound && (
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th className="text-center">Mã môn</th>
+              <th className="text-center">Tên môn</th>
+              <th className="text-center">Túi bài thi</th>
+              <th className="text-center">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="text-center text-primary" colSpan={4}><h4>Không tìm thấy túi bài thi</h4></td>
+            </tr>
+          </tbody>
+        </Table>
+      )}
+    </div>
+  );
 };
 
 export default ExportTranscript;
--- src/components/ExamResults/ReviewTestScore.js
+++ src/components/ExamResults/ReviewTestScore.js
@@ -1,9 +1,134 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { FaSearch } from "react-icons/fa";
 
 const ReviewTestScore = () => {
+    const { examId } = useParams();
+    const [examResult, setExamResult] = useState({});
+    const [subjectCode, setSubjectCode] = useState("");
+    const [studentCode, setStudentCode] = useState("");
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [reviewScore, setReviewScore] = useState("");
+
+    const fetchData = async () => {
+        setIsLoaded(false); // Reset isLoaded before fetching data
+        try {
+            const response = await axios.get(`http://localhost:5107/api/ExamResult/admin-get-by-student-code?examID=${examId}&subjectCode=${subjectCode}&studentCode=${studentCode}`);
+            setExamResult(response.data[0]);
+            setIsLoaded(true);
+        } catch (error) {
+            console.error('Error fetching exam result:', error);
+        }
+    };
+
+    const handleChangeSubjectCode = (event) => {
+        setSubjectCode(event.target.value);
+    };
+
+    const handleChangeStudentCode = (event) => {
+        setStudentCode(event.target.value);
+    };
+    const handleChangeReviewScore = (event) => {
+        var inputStringValue = event.target.value.trim();
+        if (inputStringValue.length > 0) {
+            var inputIntValue = parseFloat(inputStringValue);
+            if (inputIntValue < 0) {
+                setReviewScore('0');
+                inputIntValue = 0;
+            } else if (inputIntValue > 10) {
+                setReviewScore('10');
+                inputIntValue = 10;
+            } else {
+                setReviewScore(inputIntValue.toString());
+            }
+            var scoreResult = "" + inputIntValue;
+            const updatedExamResult = { ...examResult, averageScore: scoreResult, reviewScore: scoreResult, isReview: true };
+            setExamResult(updatedExamResult);
+        } else {
+            setReviewScore("");
+        }
+    };
+    
+    const handleSearch = () => {
+        fetchData();
+    };
+    const handleSaveReviewScore = async () => {
+        const isConfirmed = window.confirm("Bạn có chắc chắn muốn lưu điểm phúc khảo?");
+        if (isConfirmed) {
+            try {
+                const response = await axios.put(`http://localhost:5107/api/ExamResult/${examResult.examResultID}`, examResult);
+                alert(response.data)
+            } catch (error) {
+                console.error('Error fetching exam result:', error);
+                alert('Thất bại');
+            }
+        }
+    }
     return (
         <div>
-            <h1>ReviewTestScore</h1>
+            <div className='w3-row w3-margin-bottom'>
+                <div className='w3-col l2 w3-padding'>
+                    <input type='text' className='w3-input w3-border w3-border-blue w3-round' onChange={handleChangeSubjectCode} placeholder='Nhập mã môn học'/>
+                </div>
+                <div className='w3-col l2 w3-padding'>
+                    <input type='text' className='w3-input w3-border w3-border-blue w3-round' onChange={handleChangeStudentCode} placeholder='Nhập mã Sinh viên'/>
+                </div>
+                <div className='w3-col l1 w3-padding'>
+                    <button type="button" className="btn btn-outline-primary w3-padding" onClick={handleSearch}><FaSearch/></button>
+                </div>
+            </div>
+            <div className='w3-row'>
+                {isLoaded && examResult && (
+                    <>
+                        <table className='w3-table w3-table-all w3-centered'>
+                            <thead>
+                                <tr>
+                                    <th className='w3-col l2'>Số phách</th>
+                                    <th className='w3-col l2'>Điểm 1</th>
+                                    <th className='w3-col l2'>Điểm 2</th>
+                                    <th className='w3-col l2'>Điểm trung bình</th>
+                                    <th className='w3-col l2'>Nhập điểm phúc khảo</th>
+                                    <th className='w3-col l2'>Nhập điểm phúc khảo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className='w3-col l2'>{examResult.registrationCodeNumber}</td>
+                                    <td className='w3-col l2'>{examResult.examResult1}</td>
+                                    <td className='w3-col l2'>{examResult.examResult2}</td>
+                                    <td className='w3-col l2'>{examResult.averageScore}</td>
+                                    <th className='w3-col l2'>
+                                        <input type='text' className='w3-input w3-border w3-border-blue w3-round' value={reviewScore} onChange={handleChangeReviewScore}/>
+                                    </th>
+                                    <th className='w3-col l2'>
+                                        <button className='w3-button w3-white w3-border w3-border-blue w3-round w3-text-blue' onClick={handleSaveReviewScore}>Edit</button>
+                                    </th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </>
+                )}
+                {isLoaded && !examResult &&(
+                    <>
+                        <table className='w3-table-all w3-centered'>
+                            <thead>
+                                <tr>
+                                    <th>Số phách</th>
+                                    <th>Điểm 1</th>
+                                    <th>Điểm 2</th>
+                                    <th>Điểm trung bình</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td colSpan={4}><h3>Không tìm thấy dữ liệu theo yêu cầu</h3></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </>
+                )}
+            </div>
         </div>
     );
 };
